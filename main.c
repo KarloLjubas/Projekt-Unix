58a61ad66b1365fe6d2c22e3cc78bf3a7af0b0b5
KarloLjubas <77588372+KarloLjubas@users.noreply.github.com>
2022-01-23 10:59:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,8 @@
 /***************************************************************************//**
-
   @file         main.c
-
   @author       Stephen Brennan
-
   @date         Thursday,  8 January 2015
-
   @brief        LSH (Libstephen SHell)
-
 *******************************************************************************/
 
 #include <sys/wait.h>
@@ -23,7 +18,6 @@
 int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
-int myPWD(char **args);
 
 /*
   List of builtin commands, followed by their corresponding functions.
@@ -31,15 +25,13 @@ int myPWD(char **args);
 char *builtin_str[] = {
   "cd",
   "help",
-  "exit",
-  "myPWD"
+  "exit"
 };
 
 int (*builtin_func[]) (char **) = {
   &lsh_cd,
   &lsh_help,
-  &lsh_exit,
-  &lsh_myPWD
+  &lsh_exit
 };
 
 int lsh_num_builtins() {
@@ -102,24 +94,6 @@ int lsh_exit(char **args)
   @param args Null terminated list of arguments (including program).
   @return Always returns 1, to continue execution.
  */
-int myPWD(char **args){
-	char cwd[PATH_MAX];
-	if(args[1] != NULL){
-		write(STDOUT_FILENO,"\nERROR: Did not expect argument '",strlen("\nERROR: Did not expect argument '"));
-		write(STDOUT_FILENO,args[1],strlen(args[1]));
-		write(STDOUT_FILENO,"' for commmand 'pwd'\n",strlen("' for commmand 'pwd'\n"));
-	} else{
-		if(getcwd(cwd, sizeof(cwd)) != NULL){
-			write(STDOUT_FILENO,"Current working directory: ",strlen("Current working directory: "));
-			write(STDOUT_FILENO,cwd,strlen(cwd));
-			write(STDOUT_FILENO,"\n",strlen("\n"));
-		}
-		else{
-			write(STDOUT_FILENO,"\nERROR: could not load current working directory\n",strlen("\nERROR: could not load current working directory\n"));
-		}
-	}
-	return 1;
-}
 int lsh_launch(char **args)
 {
   pid_t pid;
@@ -302,4 +276,3 @@ int main(int argc, char **argv)
 
   return EXIT_SUCCESS;
 }
-
